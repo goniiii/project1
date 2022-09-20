1fccc77b1a42b59276dc85fa7420445c38d12d8a
name <gaga_1000@naver.com>
2022-09-20 11:10:07 +0900

    '네이게이션바 삽입'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <img width="100%" src="img/tomatoes.jpg"></img>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <img width="80%" src="img/cereal.jpg"></img>
     </div>
   );
 }
