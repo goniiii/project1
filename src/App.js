1cc0857d50febffc424f0586144fc9262c2ac1ff
name <gaga_1000@naver.com>
2022-09-20 12:22:08 +0900

    'Home 탭에 동영상 삽입"

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Green Table</Navbar.Brand>
+          <Navbar.Brand href="/">Green Table</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Shop</Nav.Link>
-            <Nav.Link href="#pricing">Location</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/shop">Shop</Nav.Link>
+            <Nav.Link href="/location">Location</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <img width="80%" src="img/cereal.jpg"></img>
-      <img
-        width="40px"
-        src="img/free-icon.png"
-        onClick={() => {
-          console.log("next");
-        }}
-      ></img>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              {" "}
+              <img width="80%" src="img/cereal.jpg"></img>
+              <img
+                width="40px"
+                src="img/free-icon.png"
+                onClick={() => {
+                  console.log("next");
+                }}
+              ></img>
+            </div>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <div>
+              <video width="100%" muted autoPlay loop>
+                <source src="video/forest.mp4" type="video/mp4"></source>
+              </video>
+            </div>
+          }
+        />
+        <Route
+          path="/shop"
+          element={
+            <div>
+              <p>shop 화면</p>
+            </div>
+          }
+        />
+        <Route
+          path="/location"
+          element={
+            <div>
+              <p>location 화면</p>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
