07b3353cdad90c72cee462b544ef074819a9d298
name <gaga_1000@naver.com>
2022-09-21 15:02:11 +0900

    '상품 3개 추가 총6개'

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,18 +1,63 @@
+import { Tab, Tabs, Nav } from "react-bootstrap";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 function Detail() {
+  let [tabIndex, setTabIndex] = useState(0);
+
   return (
     <div>
       <p>pd001 상세페이지</p>
       <img width="50%" height="300px" src="img/cereal.jpg"></img>
       <p>그래놀라</p>
+
       <p>그래놀라</p>
       <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
-      <p>그래놀라</p>
+      <div className="tab">
+        <Nav variant="tabs" defaultActiveKey="link1">
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link1"
+              onClick={() => {
+                setTabIndex(0);
+              }}
+            >
+              상세정보
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link2"
+              onClick={() => {
+                setTabIndex(1);
+              }}
+            >
+              리뷰
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link3"
+              onClick={() => {
+                setTabIndex(2);
+              }}
+            >
+              문의
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              eventKey="link4"
+              onClick={() => {
+                setTabIndex(3);
+              }}
+            >
+              반품/교환정보
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        {/* <Comp tabIndex={tabIndex} /> */}
+      </div>
     </div>
   );
 }
--- src/components/product.js
+++ src/components/product.js
@@ -27,6 +27,27 @@ function Product() {
         "아직도 그래놀라 유목민이세요? 매번 다른 수제 그래놀라를 찾아헤매는 나는야 그래놀라 유목민 이라구요?",
       imgPath: "img/blueberry.jpg",
     },
+    {
+      id: "pd004",
+      itemName: "그린테이블 초코 카카오 수제 그래놀라",
+      content:
+        "아직도 그래놀라 유목민이세요? 매번 다른 수제 그래놀라를 찾아헤매는 나는야 그래놀라 유목민 이라구요?",
+      imgPath: "img/muesli.jpg",
+    },
+    {
+      id: "pd005",
+      itemName: "그린테이블 초코 카카오 수제 그래놀라",
+      content:
+        "아직도 그래놀라 유목민이세요? 매번 다른 수제 그래놀라를 찾아헤매는 나는야 그래놀라 유목민 이라구요?",
+      imgPath: "img/cereal4.jpg",
+    },
+    {
+      id: "pd006",
+      itemName: "그린테이블 초코 카카오 수제 그래놀라",
+      content:
+        "아직도 그래놀라 유목민이세요? 매번 다른 수제 그래놀라를 찾아헤매는 나는야 그래놀라 유목민 이라구요?",
+      imgPath: "img/cereal5.jpg",
+    },
   ]);
   return (
     <Container>
@@ -48,6 +69,15 @@ function Product() {
                     }}
                   >
                     주문하기
+                  </Button>
+
+                  <Button
+                    variant="warning"
+                    onClick={() => {
+                      navigate("/product/" + products[i].id);
+                    }}
+                  >
+                    찜하기
                   </Button>
                 </Card.Body>
               </Card>
