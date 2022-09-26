94310824075461217830ac04e0287739dfa8523d
name <gaga_1000@naver.com>
2022-09-26 14:47:01 +0900

    'brand 페이지 작업중'

--- src/components/brand.js
+++ src/components/brand.js
@@ -1,49 +1,52 @@
 function DarkVariantExample() {
   return (
-    <div className="gt" style={{ fontFamily: "initial" }}>
+    <div>
       <p className="brand_intro">Our Story</p>
-      <div>
-        <img className="brandImg1" src="img/oats.jpg"></img>
-      </div>
-      <div className="brandText1">
-        자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+      <div className="gt" style={{ fontFamily: "initial" }}>
+        <div>
+          <img className="brandImg1" src="img/oats.jpg"></img>
+        </div>
+        <div className="brandText1">
+          <h3>Value 01</h3>
+          <h1>Explore Treasure</h1>
+          <br />
+          자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+          바쁜 아침, 통곡물의 영양을 가득 담은 그래놀라와 요거트만 있어도 건강한
+          하루를 시작하기에 충분하죠.
+        </div>
+        <div className="brandText2">
+          <h3>Value 02</h3>
+          <h1>Explore Treasure</h1>
+          <br />
+          그린테이블 그래놀라는 유기농 코코넛오일, 메이플 시럽, 비정제
+          사탕수수를 재료로 사용하여 건강한 닷맛을 낸 제품입니다. 귀리를 약 60%
+          가까이 담은 뒤 크랜베리, 아몬드 등을 각각 더해 완성했지요. 최적의 곡물
+          상태를 위해 직접 실내 온도 및 습도를 조절하고 , 귀리의 수분율과 압착
+          정도를 세심하게 관리했어요.
+        </div>
+        <div>
+          <img className="brandImg2" src="img/wheat2.jpg"></img>
+        </div>
+        <div>
+          <img className="brandImg1" src="img/oats.jpg"></img>
+        </div>
+        <div className="brandText1">
+          <h3>Value 03</h3>
+          <h1>Explore Treasure</h1>
+          <br />
+          자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+        </div>
+        <div className="brandText2">
+          <h3>Value 04</h3>
+          <h1>Explore Treasure</h1>
+          <br />
+          자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+        </div>
+        <div>
+          <img className="brandImg2" src="img/wheat2.jpg"></img>
+        </div>
       </div>
     </div>
-    // <Carousel variant="dark">
-    //   <Carousel.Item>
-    //     <img
-    //       style={{ marginLeft: "250px", height: "780px" }}
-    //       className="d-block w-75"
-    //       src="img/cereal.jpg"
-    //       alt="First slide"
-    //     />
-    //     <Carousel.Caption></Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <img
-    //       style={{ marginLeft: "250px", height: "780px" }}
-    //       className="d-block w-75"
-    //       src="img/granola.jpg"
-    //       alt="Second slide"
-    //     />
-    //   </Carousel.Item>
-
-    //   <Carousel.Item>
-    //     <img
-    //       style={{ marginLeft: "250px", height: "780px" }}
-    //       className="d-block w-75"
-    //       src="img/berry.jpg"
-    //       alt="Third slide"
-    //     />
-    //   </Carousel.Item>
-    //   {/* <Carousel.Item>
-    //     <img
-    //       className="d-block w-75"
-    //       src="img/cuisine.jpg"
-    //       alt="Fourth slide"
-    //     />
-    //   </Carousel.Item> */}
-    // </Carousel>
   );
 }
 
