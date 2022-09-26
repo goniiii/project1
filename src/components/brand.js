1f42fdbdb71f7590bece0884b530ec0dcff26783
name <gaga_1000@naver.com>
2022-09-26 15:04:19 +0900

    brand 페이지 완성

--- src/components/brand.js
+++ src/components/brand.js
@@ -16,7 +16,7 @@ function DarkVariantExample() {
         </div>
         <div className="brandText2">
           <h3>Value 02</h3>
-          <h1>Explore Treasure</h1>
+          <h1>Process</h1>
           <br />
           그린테이블 그래놀라는 유기농 코코넛오일, 메이플 시럽, 비정제
           사탕수수를 재료로 사용하여 건강한 닷맛을 낸 제품입니다. 귀리를 약 60%
@@ -28,22 +28,24 @@ function DarkVariantExample() {
           <img className="brandImg2" src="img/wheat2.jpg"></img>
         </div>
         <div>
-          <img className="brandImg1" src="img/oats.jpg"></img>
+          <img className="brandImg1" src="img/grain.jpg"></img>
         </div>
         <div className="brandText1">
           <h3>Value 03</h3>
-          <h1>Explore Treasure</h1>
+          <h1>Ingredients</h1>
           <br />
-          자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+          설탕대신 스테비올배당체를 사용해 100g당 당 함량은 3g을 넘기지 않으니
+          더욱 부담없고 단백질 까지 든든히 챙길 수 있어 더욱 만족스러울 거예요.
         </div>
         <div className="brandText2">
           <h3>Value 04</h3>
-          <h1>Explore Treasure</h1>
+          <h1>Good Taste</h1>
           <br />
-          자연 재료의 고소한 풍미를 담은 그린테이블의 그래놀라는 특별합니다.
+          3번에 걸친 저온 로스팅으로 그래놀라의 고소함과 바삭함을 극대화
+          했답니다. 그래놀라의 다양한 영양과 맛을 취향대로 준비했어요.
         </div>
         <div>
-          <img className="brandImg2" src="img/wheat2.jpg"></img>
+          <img className="brandImg2" src="img/muesli2.jpg"></img>
         </div>
       </div>
     </div>
